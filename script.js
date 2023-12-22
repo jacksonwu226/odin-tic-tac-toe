ca21908f5e8c8686315028d24fc50522a1bd8150
Jackson Wu <jacksonwu226@gmail.com>
2023-12-21 17:55:20 -0800

    Connected GUI control with game control added some styling in the box fields to look nice.

--- script.js
+++ script.js
@@ -10,9 +10,12 @@ function Cell(){
     const addToken = (player) => {
         value = player;
     }
+    const clearCell = () =>{
+        value = null;
+    }
     const isEmpty = () => !value;
     const getValue = () => value;
-    return {addToken, isEmpty, getValue};
+    return {addToken, isEmpty, getValue, clearCell};
 }
 function Gameboard(){
     const rows = 3;
@@ -25,7 +28,13 @@ function Gameboard(){
             board[i].push(Cell());
         }
     }
-
+    const clearBoard = () => {
+        for(let i = 0; i < rows; i++){
+            for(let j = 0; j < columns; j++){
+                board[i][j].clearCell();
+            }
+        }
+    }
     const getBoard = () => board;
     const addToken = (row,col, player) => {
         if(!isValidCell(row,col)){
@@ -44,8 +53,61 @@ function Gameboard(){
     const isValidCell = (row,col) =>{
         return 0<=row && row < rows && 0 <= col && col < columns && board[row][col].isEmpty();
     }
-    return {getBoard, addToken, printBoard, isValidCell};
+    return {getBoard, addToken, printBoard, isValidCell, clearBoard};
 }
+
+const GUIController = (gameController) => {
+    const init = () => {
+        cacheDom();
+        bindEvents();
+        this.gameState = gameController.isGameOver();
+        setRoundDescription();
+    };
+    const cacheDom = () => {
+        this.round_description = document.getElementById("round-description");
+        this.tic_tac_toe_field = document.querySelectorAll(".tic-tac-toe-field");
+        this.restart_btn = document.querySelector("#restart-btn");
+    };
+    const bindEvents = () => {
+        this.restart_btn.addEventListener('click', restartGame);
+        this.tic_tac_toe_field.forEach((field,index) => {
+            field.addEventListener('click', () => handleFieldClick(field,index))
+        });
+    };
+    const restartGame = () => {
+        gameController.restartGame();
+        gameState = null;
+        clearBoard();
+        setRoundDescription();
+    }
+    const clearBoard = () => {
+        this.tic_tac_toe_field.forEach((field) => {
+            field.textContent = "";
+        })
+    };
+    const handleFieldClick = (field, index) => {
+        if(!gameState){
+            const row = Math.floor(index/3);
+            const col = index % 3;
+            field.textContent= gameController.getActivePlayer().getToken();
+            gameController.playRound(row,col);
+            setRoundDescription();
+            gameState = gameController.isGameOver();
+            if(gameState){
+                setGameEndDescription();
+            }
+        }
+    };
+    const setRoundDescription = () => {
+        this.round_description.textContent = `${gameController.getActivePlayer().getName()}'s turn`;
+    }
+    const setGameEndDescription = () => {
+        this.round_description.textContent = gameState.Result;
+    } 
+    init();
+    return {init, restartGame}
+};
+
 function GameController(playerOneName = "Player 1", playerTwoName = "Player 2"){
     const players = [
         Player(playerOneName, 'X'), 
@@ -67,7 +129,7 @@ function GameController(playerOneName = "Player 1", playerTwoName = "Player 2"){
         }
         else{
             board.addToken(row,col, activePlayer);
-            const gameState = isGameOver(activePlayer, board.getBoard());
+            const gameState = isGameOver();
             if(gameState){
                 console.log(gameState)
             }
@@ -76,6 +138,10 @@ function GameController(playerOneName = "Player 1", playerTwoName = "Player 2"){
             }
         }
         printRound();
+    }
+    const restartGame = () => {
+        activePlayer = players[0];
+        board.clearBoard();
     }
     const isWinner = (player, boardState) => {
         for(let i = 0; i < 3; i++){
@@ -96,16 +162,21 @@ function GameController(playerOneName = "Player 1", playerTwoName = "Player 2"){
     const isBoardFull = (boardState) => {
         return boardState.every(row => row.every(cell => !cell.isEmpty()));
     }
-    const isGameOver = (player, boardState) => {
+    const isGameOver = () => {
+        const player = getActivePlayer();
+        const boardState = board.getBoard();
         if(isWinner(player,boardState)){
             return {Winner: player, Result: `${player.getName()} WON!`}
         }
         else if(isBoardFull(boardState)){
-            return {Winner: null, Result: `Its a draw`}
+            return {Winner: null, Result: `It's a draw`}
         }
         else{
             return null;
         }
     }
-    return {getActivePlayer, printRound, playRound, isGameOver};
+    return {getActivePlayer, printRound, playRound, isGameOver, restartGame};
 }
+
+const gameController = GameController();
+const guiController = GUIController(gameController);
